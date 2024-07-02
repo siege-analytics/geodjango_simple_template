65db31661f17feb39e06e7c10b941c3a146d1c30
Dheeraj Chand <dheeraj@dheerajchand.com>
2024-07-02 12:35:18 -0500

    1. Updating gitignore to leave Jetbrains files out 2. Added models to locations

--- app/hellodjango/hellodjango/settings.py
+++ app/hellodjango/hellodjango/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "django.contrib.gis",
+    "django.contrib.gis",       
+    "locations",
 ]
 
 MIDDLEWARE = [
