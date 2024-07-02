50962fd79bbd8a6a54502baadf4efe89ca856814
Dheeraj Chand <dheeraj@dheerajchand.com>
2024-07-02 11:52:22 -0500

    1. Spatialite seems to work for dev

--- app/hellodjango/hellodjango/settings.py
+++ app/hellodjango/hellodjango/settings.py
@@ -77,7 +77,8 @@ WSGI_APPLICATION = 'hellodjango.wsgi.application'
 
 DATABASES = {
     "default": {
-        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        #"ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.contrib.gis.db.backends.spatialite"),
         "NAME": os.environ.get("SQL_DATABASE", BASE_DIR / "db.sqlite3"),
         "USER": os.environ.get("SQL_USER", "user"),
         "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
