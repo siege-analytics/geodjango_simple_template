280b6511908fd335e47d4087661cc85d541c1361
Dheeraj Chand <dheeraj@dheerajchand.com>
2024-07-05 11:08:09 -0500

    1. Fixed .gitignore to add staticfiles back in 2. Added vector file utilities

--- app/hellodjango/hellodjango/settings/__init__.py
+++ app/hellodjango/hellodjango/settings/__init__.py
@@ -1,3 +1,4 @@
 from .django_settings import *
 from .path_settings import *
 from .logging import *
+from .vector_data_file_settings import *
---
+++ app/hellodjango/hellodjango/settings/vector_data_file_settings.py
@@ -0,0 +1,33 @@
+import os
+
+VALID_VECTOR_FILE_EXTENSIONS = [
+
+    '.dwg',
+    '.dxf',
+    '.geojson',
+    '.gpkg',
+    '.json',
+    '.kml',
+    '.kmz',
+    '.shp',
+    '.swm2',
+    '.swmaps',
+    '.swmz',
+]
+
+SHAPEFILE_EXTRA_FILES_EXTENSIONS= [
+     '.aih',
+     '.ain',
+     '.atx',
+     '.cpg',
+     '.dbf',
+     '.fbn',
+     '.fbx',
+     '.ixs',
+     '.mxs',
+     '.prj',
+     '.sbn',
+     '.sbx',
+     '.shx',
+     '.xml',
+ ]
