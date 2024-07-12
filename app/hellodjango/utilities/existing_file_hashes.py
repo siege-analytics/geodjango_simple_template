23df7392d6cd4d6577c42e0b439245a48ae03d2d
Dheeraj Chand <dheeraj@dheerajchand.com>
2024-07-12 11:14:54 -0500

    1. This is written as a dictionary but it needs to be a JSON file, will revise later

--- app/hellodjango/utilities/existing_file_hashes.py
+++ app/hellodjango/utilities/existing_file_hashes.py
@@ -1,3 +1,4 @@
 FILE_NAMES_AND_HASHES = {
-
+    'gadm_410-levels.zip': '13c3eeb6a57799930a4959094b25050f2da3a2dcf324120c9e6b33094bad8e11',
+    'timezones-with-oceans-now.shapefile.zip': 'ae9afaa12dc484ddd83476b9c14a2390f13e24029cbc7ecf12188b9a143df86e'
 }
