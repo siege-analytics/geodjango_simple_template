be990fed53a1b84537c89162d019507ab809cd1d
Dheeraj Chand <dheeraj@dheerajchand.com>
2024-07-24 21:20:52 -0500

    1. Forgot to add entrypoint revisions to last commit

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -11,6 +11,7 @@ then
     echo "PostgreSQL started"
 fi
 
+python3 hellodjango/manage.py ensure_paths
 python3 hellodjango/manage.py makemigrations
 python3 hellodjango/manage.py migrate
 python3 hellodjango/manage.py collectstatic --no-input
